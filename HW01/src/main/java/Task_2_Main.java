3cc3020fa5c5b5acbae9f4dc83f37532591a0134
csaw1433 <laura.geiger@student.uibk.ac.at>
2019-10-23 08:29:19 +0200

    changed folder

---
+++ HW01/src/main/java/Task_2_Main.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.concurrent.*;
+
+/**
+ * args[...]... InstanceType(s) (String) i.e. "t2.large t2.micro"
+ */
+
+public class Task_2_Main {
+    public static void main(final String[] args)throws Exception {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        int childID = 0;
+        ArrayList<Callable> tasks = new ArrayList<Callable>();
+        for(final String arg : args)
+        {
+            final int finalChildID = childID;
+            Callable<Void> task = new Callable<Void>()
+            {
+                @Override
+                public Void call() throws Exception
+                {
+                    String taskArgs[] = {arg, String.valueOf(finalChildID)};
+                    Task_2_Child.main(taskArgs);
+                    return null;
+                }
+            };
+
+            executor.submit(task);
+            childID++;
+        }
+
+        //cleanup executor
+       executor.shutdown();
+
+        //Waiting for child processes to terminate
+        try {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("IntterruptedExecption");
+        }
+
+    }
+
+}

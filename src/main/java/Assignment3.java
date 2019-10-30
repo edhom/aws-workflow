fe7f9b599f70329bc62a9d007c6aeb267ce0cc82
Eric Dhom <eric.dhom@icloud.com>
2019-10-30 13:31:00 +0100

    fibonacchi calculation added

--- src/main/java/Assignment3.java
+++ src/main/java/Assignment3.java
@@ -1,11 +1,24 @@
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Assignment3 {
-    public String myHandler(int myCount, Context context) {
+import java.math.BigInteger;
+
+public class Assignment3 implements RequestHandler<Integer, String> {
+    public String handleRequest(Integer myCount, Context context) {
+        String result = fib(myCount).toString();
         LambdaLogger logger = context.getLogger();
-        logger.log("received : " + myCount);
-        return String.valueOf(myCount);
+        logger.log("calculated : " + result);
+        return result;
+    }
+
+    public BigInteger fib(int n) {
+        if (n == 0){
+            return BigInteger.ZERO;
+        } else if (n == 1){
+            return BigInteger.ONE;
+        }
+        return fib(n - 2).add(fib(n - 1));
     }
 
 }

89db3df723752ec901f3503f07102a1a6fcf3211
Eric Dhom <eric.dhom@icloud.com>
2019-10-31 17:12:24 +0100

    interface loesung statt invokerequest

--- src/main/java/Ass3_main.java
+++ src/main/java/Ass3_main.java
@@ -3,10 +3,10 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.AWSLambda;
 import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
-import com.amazonaws.services.lambda.model.InvokeRequest;
-import com.amazonaws.services.lambda.model.InvokeResult;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.amazonaws.services.lambda.invoke.LambdaInvokerFactory;
 
+import java.math.BigInteger;
+/*
 public class Ass3_main {
     public static void main(String[] args) {
         InvokeRequest invokeRequest = new InvokeRequest()
@@ -31,5 +31,22 @@ public class Ass3_main {
         System.out.println(invokeResult.getStatusCode());
     }
 }
+*/
 
+public class Ass3_main {
+    public static void main(String[] args) {
+
+        BasicAWSCredentials awsCredentials = GeneralUtils.loadCredentialsFromConfig();
+        AWSLambda awsLambda = AWSLambdaClientBuilder.standard()
+                .withRegion(Regions.EU_CENTRAL_1)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
+
+        final Ass3_lambdaSingleService lambdaService = LambdaInvokerFactory.builder()
+                .lambdaClient(awsLambda)
+                .build(Ass3_lambdaSingleService.class);
+
+        String input = "[0,1,2,3,4,5,6,7,8,9]";
+        BigInteger[] result = lambdaService.calc_fib(input);
+    }
+}
 

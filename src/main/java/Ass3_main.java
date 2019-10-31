31e08a9a30c2747c8a9d71fa9b7d16f63b03c33d
csaw1433 <laura.geiger@student.uibk.ac.at>
2019-10-31 17:13:16 +0100

    pom und main changed

--- src/main/java/Ass3_main.java
+++ src/main/java/Ass3_main.java
@@ -7,17 +7,25 @@ import com.amazonaws.services.lambda.model.InvokeRequest;
 import com.amazonaws.services.lambda.model.InvokeResult;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+
 public class Ass3_main {
     public static void main(String[] args) {
         InvokeRequest invokeRequest = new InvokeRequest()
-                .withFunctionName("Fibonacchi")
+                .withFunctionName("Fibonacci")
                 .withPayload("[0,1,2,3,4,5,6,7,8,9]");
 
         BasicAWSCredentials awsCredentials = GeneralUtils.loadCredentialsFromConfig();
 
-        AWSLambda awsLambda = AWSLambdaClientBuilder.standard()
-                .withRegion(Regions.EU_CENTRAL_1)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
+
+        AWSLambdaClientBuilder builder = AWSLambdaClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.EU_CENTRAL_1);
+        //AWSLambda builder = AWSLambdaClientBuilder.standard()
+               // .withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
+                //.withRegion(Regions.EU_CENTRAL_1);
+
+
+        AWSLambda awsLambda = builder.build();
 
         InvokeResult invokeResult = null;
 

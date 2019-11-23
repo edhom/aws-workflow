2d3f74639c6fcf7549736d389f0b499c8d42fd0f
Laura Geiger <laura.geiger@student.uibk.ac.at>
2019-11-23 09:46:04 +0100

    simple parser for string to integer list

---
+++ src/main/java/Project/StringToIntegerList.java
@@ -0,0 +1,27 @@
+package Project;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringToIntegerList {
+
+    /**
+     *
+     * @param string for coordinates
+     * @return List of Integers with coordinates
+     */
+    public static List<Integer> buildIntegerArray(String string){
+
+        List<Integer> intArray = new ArrayList<>();
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(string);
+        while(m.find()) {
+            System.out.println(m.group());
+            intArray.add(Integer.parseInt(m.group()));
+        }
+        return intArray;
+    }
+}

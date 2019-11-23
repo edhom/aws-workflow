bf15d12d53f35c012deec8c8434abfc974399e77
Eric Dhom <eric.dhom@icloud.com>
2019-11-23 17:13:42 +0100

    f2, f4, f6, f8. Du musst nur das S3 Bucket gegen deins tauschen:)

--- Eric_Funktionen/f6_optimalRideRequest.py
+++ Eric_Funktionen/f6_optimalRideRequest.py
@@ -1,7 +1,7 @@
 import json
 
 def score(req):
-    return req['Profit'] - req['Overhead'] * 0.30 - req['OverheadinTime'] * 10
+    return req['Profit'] - req['Overhead'] * 0.30 - req['OverheadInTime'] * 10
 
 def lambda_handler(requests, context):
     

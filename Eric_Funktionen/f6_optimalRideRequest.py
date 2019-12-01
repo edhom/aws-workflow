e7991bc9076235c940f1d351a454bf83313faed8
Eric Dhom <eric.dhom@icloud.com>
2019-12-01 12:18:37 +0100

    Updated f6 to consider only matching requests

--- Eric_Funktionen/f6_optimalRideRequest.py
+++ Eric_Funktionen/f6_optimalRideRequest.py
@@ -5,8 +5,13 @@ def score(req):
 
 def lambda_handler(requests, context):
     
-    optimalMatch = requests[0]
-    for req in requests:
-        optimalMatch = req if score(req) > score(optimalMatch) else optimalMatch
+    matchingReqs = list(filter(lambda req: req['match'] == True, requests))
     
+    if len(matchingReqs) > 0:
+        optimalMatch = matchingReqs[0]
+        for req in matchingReqs:
+            optimalMatch = req if score(req) > score(optimalMatch) else optimalMatch
+    else:
+        optimalMatch = None
+        
     return optimalMatch
